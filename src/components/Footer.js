a2644aaa5fcd26a50551da29ba709561ecb61124
Dieudonnee <75275692+Dieudonnee@users.noreply.github.com>
2022-03-16 15:37:35 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import Style from './footer.module.scss'
+import classNames from 'classnames'
 
 export default function Footer() {
   return (
-    <footer>
+    <footer className={classNames(Style.footer)}>
               <p><a target="_blank" href='https://www.linkedin.com/in/dieudonne-developer/' rel="noopener noreferrer">Template made with &hearts; by Dodos</a></p>
               <p>&copy; 2022</p>
           </footer>
