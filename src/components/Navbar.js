92e4f2748e2431a9c62030001e3f9f74e7778d22
Dieudonnee <75275692+Dieudonnee@users.noreply.github.com>
2022-03-16 14:16:21 +0200

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {NavLink} from "react-router-dom"
 import Style from './Navbar.module.scss'
-import Toggler from "./Toggler"
+
 import ClassNames from "classnames";
 import {info} from "../Info/info";
 
